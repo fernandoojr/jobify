21f746c01b86c0706bb6b49993d248841c708ae6
fernandofojr <fernandofojr@gmail.com>
2020-10-21 11:43:35 -0300

    arrumado nome para 'views'

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use('/admin', (req, res, next) => {
     }
 })
 
-app.set('views', path.join(__dirname, 'view'))
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname,'public')))
 app.use(bodyParser.urlencoded({ extended: true }))
