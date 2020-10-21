27da8ccc9e8a00320103a7d88e4a1619c83d62e3
fernandofojr <fernandofojr@gmail.com>
2020-10-21 11:38:43 -0300

    Bloqueando admin se não for local

--- index.js
+++ index.js
@@ -8,12 +8,19 @@ const sqlite = require('sqlite-sync')
 
 const port = process.env.PORT || 3000
 
-dbConnection = sqlite.connect(path.resolve(__dirname,'banco.sqlite'));
+const dbConnection = sqlite.connect(path.resolve(__dirname,'banco.sqlite'));
 
+app.use('/admin', (req, res, next) => {
+    if(req.hostname === 'localhost'){
+        next()
+    }else{
+        res.send('Not allowed')
+    }
+})
 
 app.set('views', path.join(__dirname, 'view'))
 app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname,'public')))
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', async(request, response) => {
